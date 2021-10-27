8438b1fbec2627bdd4857309c474ca7848d6958d
Paulo Freire <paulofreire@paulofreire.biz>
2021-10-27 10:41:38 -0300

    New version 1.0.8

--- i4ever_gestao/__manifest__.py
+++ i4ever_gestao/__manifest__.py
@@ -11,7 +11,7 @@ envolvidas, além de garantir uma maior qualidade nas tarefas desempenhadas.
     'author': "Paulo Roberto G. Freire",
     'website': "http://focosolution.com",
     'category': 'Image4ever',
-    'version': '1.0.7',
+    'version': '1.0.8',
     'depends': ['base'],
     'data': [
         'security/groups.xml',
